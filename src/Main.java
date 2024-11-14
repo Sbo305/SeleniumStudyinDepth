853985ab1a7ca1d7dd2f01042dac4d291b0656a9
sbongisenindwandwe <sbongisenindwandwe@gmail.com>
2024-11-14 20:24:15 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/sbongisenindwandwe/Documents/JavaProjects/SeleniumStudyinDepth/Drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Navigating to BrowserStack website
+
+
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com");
+
+        driver.findElement(By.linkText("Basic Auth")).click();
+
+
+
+
+
+        Thread.sleep(5000);
+        System.out.println("Successfully launched Chrome on fullscreen mode!");
+
+        driver.close();
     }
 }
